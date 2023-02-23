c65346c11b48ef9d9bc59c6d3980bef8c130d5d3
celsovillano <celsovillano@gmail.com>
2023-02-24 05:39:35 +0800

    added fibonacci module

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci_seq(nth_term=10):
+    fib_list = [0,1]
+    
+    # Get the nth term of the fibonacci sequence
+    for k in range(nth_term-2):
+        new_fib = fib_list[k] + fib_list[k+1]
+        fib_list.append(new_fib)
+
+    return fib_list[-1]
