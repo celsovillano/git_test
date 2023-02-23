7cedcb35cae21bd460165701350f8eb13393c4a6
celsovillano <celsovillano@gmail.com>
2023-02-24 05:46:41 +0800

    Fix indexing of Fibonacci number

--- fibonacci.py
+++ fibonacci.py
@@ -2,7 +2,7 @@ def fibonacci_seq(nth_term=10):
     fib_list = [0,1]
     
     # Get the nth term of the fibonacci sequence
-    for k in range(nth_term-2):
+    for k in range(nth_term):
         new_fib = fib_list[k] + fib_list[k+1]
         fib_list.append(new_fib)
 
